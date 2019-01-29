3b0e4af72444a394141c0fb5bd070c91473ea287
cesarvh <cesarv.h@berkeley.edu>
2019-01-29 12:46:02 -0800

    yuum

--- services/batch/service/src/main/java/org/collectionspace/services/batch/nuxeo/UpdateObjectNationalitiesFromPersonBatchJob.java
+++ services/batch/service/src/main/java/org/collectionspace/services/batch/nuxeo/UpdateObjectNationalitiesFromPersonBatchJob.java
@@ -36,13 +36,16 @@ public class UpdateObjectNationalitiesFromPersonBatchJob extends AbstractBatchJo
         // These are the CSIDs of all the documents that we need to update
         for (String csid : collectionObjectsList) {
             PoxPayloadOut collectionObjectPayload = findCollectionObjectByCsid(csid);
-            String inAuthority = getFieldValue(collectionObjectPayload, "collectionobjects_common", "inAuthority");
+            // String inAuthority = getFieldValue(collectionObjectPayload, "collectionobjects_common", "inAuthority");
 
             // Why is this a String??
-            String oldNationalities = getFieldValue(collectionObjectPayload, "collectionobjects_bampfa", "nationalities");
-            updateNationalities(oldNationalities, nationalitiesToUpdate, csid, inAuthority);
+            // collectionObjectPayload.getPart("collectionobjects_bampfa").asElement().selectNodes("nationalities/nationality").getText()
+            List<String> oldNationalities = getFieldValues(collectionObjectPayload, "collectionobjects_bampfa", "nationalities/nationality");
+            try {
+            updateNationalities(oldNationalities, nationalitiesToUpdate, csid);
+            } catch (Exception e) {
 
-
+            }
 
         }
 
@@ -58,37 +61,38 @@ public class UpdateObjectNationalitiesFromPersonBatchJob extends AbstractBatchJo
         return null;
     }
 
-    public void updateNationalities(String collectionObjNationalities, Map<String, List> nationalitiesToUpdate, String objCsid, String authorityCsid) {
+    public void updateNationalities(List<String> collectionObjNationalities, Map<String, List> nationalitiesToUpdate, String objCsid) throws URISyntaxException {
         List nationalitiesToAdd = nationalitiesToUpdate.get("add");
         List nationalitiesToDelete = nationalitiesToUpdate.get("delete");
 
-        // collectionObjNationalities.addAll(nationalitiesToAdd);
-        // collectionObjNationalities.removeAll(nationalitiesToDelete);
+        collectionObjNationalities.addAll(nationalitiesToAdd);
+        collectionObjNationalities.removeAll(nationalitiesToDelete);
 
-        // // So now collectionObjNationalities is the most updated version of what we need to have. Time to assemble them
-        // String nationalitiesString = "";
-        // for (String n : collectionObjNationalities) {
-        //     nationalitiesString += "<nationality>" + n + "</nationality>\n";
-        // }
+        // So now collectionObjNationalities is the most updated version of what we need to have. Time to assemble them
+        String nationalitiesString = "";
+        for (String n : collectionObjNationalities) {
+            nationalitiesString += "<nationality>" + n + "</nationality>\n";
+        }
 
         
-        // String nationalitiesGroupString  = "<nationalities>" + 
-        //                             nationalitiesString + 
-        //                             "</nationalities>";
+        String nationalitiesGroupString  = "<nationalities>" + 
+                                    nationalitiesString + 
+                                    "</nationalities>";
 
 
-        // String updatePayload = 
-        //     "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
-        //     "<document name=\"collectionobjects\">" +
-        //     "<ns2:collectionobjects_bampfa xmlns:ns2=\"http://collectionspace.org/services/collectionobject/local/bampfa\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
-        //     nationalitiesGroupString + 
-        //     "</ns2:collectionobjects_bampfa>" +
-        //     "</document>";
+        String updatePayload = 
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+            "<document name=\"collectionobjects\">" +
+            "<ns2:collectionobjects_bampfa xmlns:ns2=\"http://collectionspace.org/services/collectionobject/local/bampfa\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">" +
+            nationalitiesGroupString + 
+            "</ns2:collectionobjects_bampfa>" +
+            "</document>";
 
-        // // Now update????
+        // Now update????
     
-		// AuthorityResource<?, ?> resource = (AuthorityResource<?, ?>) getResourceMap().get("personauthorities");
-		// resource.updateAuthorityItem(getServiceContext(), getResourceMap(), createUriInfo(), authorityCsid, objCsid, updatePayload);
+		AuthorityResource<?, ?> resource = (AuthorityResource<?, ?>) getResourceMap().get("personauthorities");
+		// resource.updateAuthorityItem(getServiceContext(), getResourceMap(), createUriInfo(), authorityCsid, objCsid, , updatePayload);
+		resource.updateAuthorityItem(getServiceContext(), getResourceMap(), createUriInfo(), objCsid, objCsid, updatePayload);
 
 
     }
